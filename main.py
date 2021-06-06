bcf2d88a9a4a76b2d3e4de0c931488854433cd5e
SherwayneBenjam <>
2021-06-06 18:00:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+#Data Types
+
+#string
+
+print("Hello"[0])
+print("Hello"[1])
+print("Hello"[2])
+print("Hello"[3])
+print("Hello"[4])
+
+#Interger
+print(123 + 456)
+print(1000 * 0.02)
+print(50 * 1250)
+#Float
+print(34563.79)
+print(7.899854)
+
+#Boolean
+True
+False
+
+num_char = len(input("What is your name?"))
+new_num_char = str(num_char)
+
+print("Your name has " + new_num_char + " characters.")
+
+a = float(123)
+print(type(a))
+
+print(70 + float("100.5"))
+
+print(str(70)+ str(100))
